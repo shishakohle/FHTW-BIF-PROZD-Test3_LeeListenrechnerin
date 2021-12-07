46746efd3bbb12888190787ae3f7da16af7983b4
Ingo Weigel <me19m022@technikum-wien.at>
2021-12-07 04:02:01 +0100

    enter the list, prepare evaluation of list

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 enum nodeType
 {
-	operator;
-	integer;
+	operator_type,
+	number_type
 };
 
 struct node
@@ -35,11 +35,28 @@ int isEmpty(struct list *list)
 	return list->head == NULL ? 1 : 0;
 }
 
-void addLast(struct list *list, int val)
+void addOperator(struct list *list, char operator)
 {
 	// create new node with val
 	struct node *newNode = (struct node*)malloc( sizeof(struct node) );
-	newNode->val  = val;
+	newNode->type      = operator_type;
+	newNode->operator  = operator;
+	newNode->next      = NULL; // as newNode will become last item on the list
+	
+	// make newNode the new tail in the list
+	if ( !isEmpty(list) )
+		list->tail->next = newNode;
+	else
+		list->head = newNode;
+	list->tail = newNode;
+}
+
+void addNumber(struct list *list, int number)
+{
+	// create new node with val
+	struct node *newNode = (struct node*)malloc( sizeof(struct node) );
+	newNode->type = number_type;
+	newNode->val  = number;
 	newNode->next = NULL; // as newNode will become last item on the list
 	
 	// make newNode the new tail in the list
@@ -50,20 +67,20 @@ void addLast(struct list *list, int val)
 	list->tail = newNode;
 }
 
-void add(struct list *list, int val)
-{
-	addLast(list, val);
-}
-
 void printList(struct list *list)
 {
 	struct node *current = list->head;
 
 	while( current != NULL )
 	{
-		printf("%i ", current->val);
+		if (current->type==number_type)
+			printf("%i", current->val);
+		else if (current->type==operator_type)
+			printf("%c", current->operator);
 		current = current->next;
 	}
+	
+	printf("\n");
 }
 
 void clearList(struct list *list)
@@ -78,4 +95,53 @@ void clearList(struct list *list)
 	}
 	
 	free(list);
+}
+
+void evaluate(struct list *list)
+{
+	
+}
+
+int isValidOperator(char c)
+{
+	return c=='+' || c=='-' || c=='*' || c=='/' || c=='=' ? 1:0;
+}
+
+int main(void)
+{
+	struct list* lee = initList();
+	
+	int number;
+	char operator;
+	
+	do
+	{
+		printf("\nEnter number: ");
+		scanf("%i", &number);
+		addNumber(lee, number);
+		
+		printf("\nCurrent list: ");
+		printList(lee);
+		
+		do
+		{
+			printf("\nEnter operator: ");
+			scanf(" %c", &operator);
+		}
+		while ( !isValidOperator(operator) );
+		
+		if (operator != '=')
+		{
+			addOperator(lee, operator);
+			printf("\nCurrent list: ");
+			printList(lee);
+		}
+	}
+	while ( operator != '=' );
+	
+	evaluate(lee);
+	
+	clearList(lee);
+	
+	return 0;
 }
